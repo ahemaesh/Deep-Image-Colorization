105eb6c976907d37da1a03695269fe521e7e995b
ahemaesh <ahemaesh@andrew.cmu.edu>
2019-11-09 14:38:51 -0500

    minor changes

--- colorization/training_utils.py
+++ colorization/training_utils.py
@@ -11,59 +11,41 @@ matplotlib.use('Agg')
 matplotlib.rcParams['figure.figsize'] = (10.0, 4.0)
 import matplotlib.pyplot as plt
 
-def evaluation_pipeline(col, number_of_images):
-    # Set up validation (input queues, graph)
-    irr = LabImageRecordReader('val_lab_images_*.tfrecord', dir_tfrecord)
-    read_batched_examples = irr.read_batch(number_of_images, shuffle=False)
-    imgs_l_val = read_batched_examples['image_l']
-    imgs_true_ab_val = read_batched_examples['image_ab']
-    imgs_emb_val = read_batched_examples['image_embedding']
-    imgs_ab_val = col.build(imgs_l_val, imgs_emb_val)
-    cost, summary = loss_with_metrics(imgs_ab_val, imgs_true_ab_val,
-                                      'validation')
-    return {
-        'imgs_l': imgs_l_val,
-        'imgs_ab': imgs_ab_val,
-        'imgs_true_ab': imgs_true_ab_val,
-        'imgs_emb': imgs_emb_val,
-        'cost': cost,
-        'summary': summary
-    }
 
+# Avinash : May be uncomment later for viewing
 
+# def plot_evaluation(res, folder_num, epoch):
+#     maybe_create_folder(join(dir_root, 'images', folder_num))
+#     for k in range(len(res['imgs_l'])):
+#         img_gray = l_to_rgb(res['imgs_l'][k][:, :, 0])
+#         img_output = lab_to_rgb(res['imgs_l'][k][:, :, 0],
+#                                 res['imgs_ab'][k])
+#         img_true = lab_to_rgb(res['imgs_l'][k][:, :, 0],
+#                               res['imgs_true_ab'][k])
+#         top_5 = np.argsort(res['imgs_emb'][k])[-5:]
+#         try:
+#             top_5 = ' / '.join(labels_to_categories[i] for i in top_5)
+#         except:
+#             ptop_5 = str(top_5)
 
-def plot_evaluation(res, folder_num, epoch):
-    maybe_create_folder(join(dir_root, 'images', folder_num))
-    for k in range(len(res['imgs_l'])):
-        img_gray = l_to_rgb(res['imgs_l'][k][:, :, 0])
-        img_output = lab_to_rgb(res['imgs_l'][k][:, :, 0],
-                                res['imgs_ab'][k])
-        img_true = lab_to_rgb(res['imgs_l'][k][:, :, 0],
-                              res['imgs_true_ab'][k])
-        top_5 = np.argsort(res['imgs_emb'][k])[-5:]
-        try:
-            top_5 = ' / '.join(labels_to_categories[i] for i in top_5)
-        except:
-            ptop_5 = str(top_5)
+#         plt.subplot(1, 3, 1)
+#         plt.imshow(img_gray)
+#         plt.title('Input (grayscale)')
+#         plt.axis('off')
+#         plt.subplot(1, 3, 2)
+#         plt.imshow(img_output)
+#         plt.title('Network output')
+#         plt.axis('off')
+#         plt.subplot(1, 3, 3)
+#         plt.imshow(img_true)
+#         plt.title('Target (original)')
+#         plt.axis('off')
+#         plt.suptitle(top_5, fontsize=7)
 
-        plt.subplot(1, 3, 1)
-        plt.imshow(img_gray)
-        plt.title('Input (grayscale)')
-        plt.axis('off')
-        plt.subplot(1, 3, 2)
-        plt.imshow(img_output)
-        plt.title('Network output')
-        plt.axis('off')
-        plt.subplot(1, 3, 3)
-        plt.imshow(img_true)
-        plt.title('Target (original)')
-        plt.axis('off')
-        plt.suptitle(top_5, fontsize=7)
-
-        plt.savefig(join(
-            dir_root, 'images', run_id, '{}_{}.png'.format(epoch, k)))
-        plt.clf()
-        plt.close()
+#         plt.savefig(join(
+#             dir_root, 'images', run_id, '{}_{}.png'.format(epoch, k)))
+#         plt.clf()
+#         plt.close()
 
 def l_to_rgb(img_l):
     """
