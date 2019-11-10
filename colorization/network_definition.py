9a36b9a1b46947b633084db7783a2340108d83aa
ahemaesh <ahemaesh@andrew.cmu.edu>
2019-11-09 19:41:51 -0500

    working till inception_model

--- colorization/network_definition.py
+++ colorization/network_definition.py
@@ -14,14 +14,13 @@ class Colorization(nn.Module):
     def __init__(self, depth_after_fusion):
         super(Colorization,self).__init__()
         self.encoder = Encoder()
-
         self.fusion = FusionLayer()
-        self.after_fusion = nn.Conv2D(in_channels=1256, out_channels=depth_after_fusion,kernel_size=3, stride=1,padding=1)
+        self.after_fusion = nn.Conv2d(in_channels=1256, out_channels=depth_after_fusion,kernel_size=3, stride=1,padding=1)
         self.decoder = Decoder(depth_after_fusion)
 
-        self.encoder.apply(init_weights)
-        self.fusion.apply(init_weights)
-        self.decoder.apply(init_weights)
+        # self.encoder.apply(init_weights)
+        # self.fusion.apply(init_weights)
+        # self.decoder.apply(init_weights)
 
     def forward(self, img_l, img_emb):
         img_enc = self.encoder(img_l)
